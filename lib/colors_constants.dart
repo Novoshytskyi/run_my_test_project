88096fedd9701d274e516cf2f1271d42cefc6074
Anton Novoshytskyi <novtsm@gmail.com>
2023-07-08 17:57:58 +0300

    Цвета вынесены в colors_constants.dart. Выполнена заливка контейнеров с штриховым контуром.

---
+++ lib/colors_constants.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+const bottomTabBarColor = Color(0xFF111016);
+
+const dottedBorderColor = Color(0xFF37616F);
+
+const dottedBorderContainerColor = Color(0xFF111016);
+
+const mainEmptyPageBackgroundColor = Color(0xFF15181E);
+
+const progressBarContainerColor = Color(0xFF111016);
+
+const startButtonBackgroundColor = Color(0xFF1DB9DD);
+
+const svgIconWithShadowButtonShadowColor = Color(0xFF9E9E9E);
+
+const textColor = Color(0xFFFFFFFF);
+
+const iconColorFilter = ColorFilter.mode(
+  Color(0xFFFFFFFF),
+  BlendMode.srcIn,
+);
+
+const addIconColorFilter = ColorFilter.mode(
+  Color(0xFF1DB9DD),
+  BlendMode.srcIn,
+);
